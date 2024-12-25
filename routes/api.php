05ea257d65281db5435783a825e7dc33929bd79f
Prabin <manandharprabin819@gmail.com>
2024-12-25 09:52:57 +0545

    making APIs

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/posts', function(){    //  /api/posts
+    return response()->json([
+        'posts' => [
+            [
+                'title' => 'Post One'
+            ]
+        ]
+    ]);
+});
