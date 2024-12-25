8d7016124f45320de74e28f7a82070d0c84deb4a
Prabin <manandharprabin819@gmail.com>
2024-12-25 14:18:10 +0545

    db populating data by factory and our own data in db

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,29 +2,9 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-            'id' => 1,
-            'title' => 'Listing one',
-            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-            ],
-            [
-            'id' => 2,
-            'title' => 'Listing two',
-            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    //
 }
