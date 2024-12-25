6e3fda48515e3d09cdb65fccd655a70fcfe32bd0
Prabin <manandharprabin819@gmail.com>
2024-12-25 16:14:19 +0545

    working with views

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,8 +1,16 @@
-<h1>Listings</h1>
-<br>
-<h1> {{ $heading }}</h1>
-<br>
-@foreach($listings as $listing)
-    <a href="/listings/{{ $listing['id'] }}"><h2>{{ $listing['title'] }}</h2></a>
-    <p>{{ $listing['description'] }}</p>
-@endforeach
+@extends('layout')
+
+@section('content')
+
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        @unless (count($listings) == 0)
+        @foreach($listings as $listing)
+            <a href="/listings/{{ $listing['id'] }}"><h2>{{ $listing['title'] }}</h2></a>
+            <p>{{ $listing['description'] }}</p>
+        @endforeach
+        @else
+        <p>No listings found</p>
+        @endunless
+    </div>
+
+@endsection
